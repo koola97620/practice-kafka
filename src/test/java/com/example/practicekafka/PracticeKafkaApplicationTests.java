62f0bef6d733714c40e9c0754fb6ce16377b3e59
최재용 <jdragon@i-screamedu.co.kr>
2022-08-31 15:30:41 +0900

    Initial commit

---
+++ src/test/java/com/example/practicekafka/PracticeKafkaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.practicekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PracticeKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
